a663fbbef35497228c4d7082443fcca2c18e4472
Lenty Chang <lentychang@gmail.com>
2018-08-09 17:09:49 +0200

    add function find_closest_normal_pair

--- src/plane_detection.py
+++ src/plane_detection.py
@@ -2,31 +2,17 @@
 
 from dataIO import read_step_file, Display
 from topo2 import RecognizeTopo
-from OCC.Display.SimpleGui import init_display
 import os.path
 import logging
 from OCC.BRepAdaptor import BRepAdaptor_Surface, BRepAdaptor_Curve
 import ipdb
+from OCC.gp import gp_Ax1, gp_Pnt, gp_Dir, gp_Trsf, gp_Ax3, gp_Pln
+from math import degrees
+from OCC.BRepBuilderAPI import BRepBuilderAPI_MakeFace
 from OCC.TopLoc import TopLoc_Location    
 from OCC.GProp import GProp_GProps
 from OCC.BRepGProp import brepgprop_VolumeProperties
 
-if __name__ == '__main__':
-
-    ##############################
-    logging.basicConfig(filename="logging.txt", filemode='w',
-                        level=logging.warning)
-    run_display = True
-    if(run_display):
-        display, start_display, add_menu, add_function_to_menu = init_display()
-        display.SetSelectionModeEdge()
-        # display.register_select_callback(click_edge)
-        # first loads the STEP file and display
-    fileList = ['lf064-01.stp', 'cylinder_group_test.stp', 'cylinder_cut.stp',
-                'cylinder_cut2.stp', 'face_recognition_sample_part.stp',
-                'cylinder_with_side_hole.stp', 'cylinder_with_side_slot.stp',
-                'cylinder_with_slot.stp', 'cylinders.stp']
-    shapeFromModel = read_step_file(os.path.join('..', 'models', fileList[6]))
 
 def group_planes_by_axis(shape):
     """[summary]
@@ -79,15 +65,79 @@ def centerOfMass(solid):
     brepgprop_VolumeProperties(solid, prop)
     return prop.CentreOfMass()
     
-    ipdb.set_trace()
 
-    if(run_display):
-        display.DisplayShape(shapeFromModel, update=True)
-        add_menu('recognition')
-        # add_function_to_menu('recognition', recognize_batch)
-        add_menu('Selection Mode')
-        # add_function_to_menu('Selection Mode', select_edge)
-        # add_function_to_menu('Selection Mode', select_face)
+def find_closest_normal_pair(normal_add, normal_base=None, tolerance=0.5):
+    """[summary]
+    Return the plane pairs with least difference of orientation of normal.
+    If normal_base is not given, then take the default XY, YZ, ZX planes
 
-        start_display()
-    ##############################
+    Arguments:
+        normal_add {dictionary} -- {'normalOfPlane':plane}
+                                     normalOfPlane{string}
+                                     plane{list of TopoDS_Shape}
+        normal_base {dictionary} -- same as normal_base
+
+    Keyword Arguments:
+        tolerance {float} -- content: float
+                             the angle within this tolerance [deg] will be consider as the same set. (default: {0.5})
+
+    Returns:
+        {dictionary} -- content: {string:TopoDS_Shape}
+    """
+    # should consider Z axis first?
+    if normal_base is None:
+        plnXY = gp_Pln(gp_Ax3(gp_Pnt(0.0, 0.0, 0.0), gp_Dir(0.0, 0.0, 1.0)))
+        plnYZ = gp_Pln(gp_Ax3(gp_Pnt(0.0, 0.0, 0.0), gp_Dir(1.0, 0.0, 0.0)))
+        plnZX = gp_Pln(gp_Ax3(gp_Pnt(0.0, 0.0, 0.0), gp_Dir(0.0, 1.0, 0.0)))
+        normal_base = {'0.000,0.000,1.000': [BRepBuilderAPI_MakeFace(plnXY).Face()],
+                       '1.000,0.000,0.000': [BRepBuilderAPI_MakeFace(plnYZ).Face()],
+                       '0.000,1.000,0.000': [BRepBuilderAPI_MakeFace(plnZX).Face()]}
+    keys_base = normal_base.keys()
+    keys_add = normal_add.keys()
+    tol = tolerance  # [deg]
+    minPair = {'minVal': 90.0, 'minPair': []}
+
+    for key_base in keys_base:
+        surf_base = BRepAdaptor_Surface(normal_base[key_base][0], True)
+        ax1_normal_base = surf_base.Plane().Axis()
+        for key_add in keys_add:
+            surf_add = BRepAdaptor_Surface(normal_add[key_add][0], True)
+            ax1_normal_add = surf_add.Plane().Axis()
+            ang = degrees(ax1_normal_base.Angle(ax1_normal_add))
+            complementary_angle = 180.0 - ang
+            min_ang = min(ang, complementary_angle)
+            if abs(min_ang - minPair['minVal']) <= tol:
+                minPair['minPair'].append([key_base, key_add])
+            else:
+                if minPair['minVal'] - min_ang >= tol:
+                    minPair['minPair'] = [[key_base, key_add]]
+                    minPair['minVal'] = min_ang
+    return minPair
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename="logging.txt", filemode='w',
+                        level=logging.warning)
+    fileList = ['lf064-01.stp', 'cylinder_group_test.stp', 'cylinder_cut.stp',
+                'cylinder_cut2.stp', 'face_recognition_sample_part.stp',
+                'cylinder_with_side_hole.stp', 'cylinder_with_side_slot.stp',
+                'cylinder_with_slot.stp', 'cylinders.stp', 'compound_solid_face-no-contact.stp',
+                'lf064-02.stp']
+    shapeFromModel = read_step_file(os.path.join('..', 'models', fileList[9]))
+    shp_topo = RecognizeTopo(shapeFromModel)
+    solid1 = shp_topo.solids[0]
+    solid2 = shp_topo.solids[1]
+
+    pnt1 = centerOfMass(solid1)
+    pnt2 = centerOfMass(solid2)
+
+    normal_group1 = group_planes_by_axis(solid1)
+    normal_group2 = group_planes_by_axis(solid2)
+
+    ipdb.set_trace(context=10)
+    ang_list = find_closest_normal_pair(normal_group2)
+    showShapes = shapeFromModel
+    frame = Display(solid2, run_display=True)
+    ipdb.set_trace(context=10)
+
+
