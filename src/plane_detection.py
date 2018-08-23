5dfc66d1538b72389cba15aba4d0442e08eb11c5
Lenty Chang <lentychang@gmail.com>
2018-08-23 14:20:55 +0200

    refactor

--- src/plane_detection.py
+++ src/plane_detection.py
@@ -12,6 +12,8 @@ from OCC.BRepBuilderAPI import BRepBuilderAPI_MakeFace
 from OCC.TopLoc import TopLoc_Location
 from OCC.GProp import GProp_GProps
 from OCC.BRepGProp import brepgprop_VolumeProperties
+from OCC.AIS import ais_ProjectPointOnPlane
+import numpy as np
 
 
 def group_planes_by_axis(shape):
@@ -31,18 +33,13 @@ def group_planes_by_axis(shape):
     for pln in planeList:
         gp_pln = BRepAdaptor_Surface(pln).Plane()
         normal = gp_pln.Axis().Direction()
-        key = '%.6f,%.6f,%.6f' % (normal.X(), normal.Y(), normal.Z())
+        key = '%.6f,%.6f,%.6f' % (round(normal.X(), 6), round(normal.Y(), 6), round(normal.Z(), 6))
+        key = tuple([float(i) for i in key.split(',')])
         if key not in pln_dict.keys():
             pln_dict[key] = [pln]
         else:
             pln_dict[key].append(pln)
     return pln_dict
-
-
-def list2gpVec(ls):
-    if len(ls) != 3:
-        print('Error, the length of the input list should be 3, [float x, float y, float z]')
-    return gp_Vec(ls[0], ls[1], ls[2])
 
 
 def mv2CMass(shp, pnt):
@@ -77,54 +74,115 @@ def centerOfMass(solid):
     return prop.CentreOfMass()
 
 
-def find_closest_normal_pair(normal_add, normal_base=None, tolerance=0.5):
+def find_closest_normal_pair(solid_add, solid_base=None, ang_tol=0.5):
     """[summary]
     # [ToDo] sort by angle difference
-    Return the plane pairs with least difference of orientation of normal.
-    If normal_base is not given, then take the default XY, YZ, ZX planes
+    Return the direction pairs with least difference of orientation of normal.
+    If solid_base is not given, then take the default XY, YZ, ZX planes
+
 
     Arguments:
-        normal_add {dictionary} -- {'normalOfPlane':plane}
-                                     normalOfPlane{string}
-                                     plane{list of TopoDS_Shape}
-        normal_base {dictionary} -- same as normal_base
+        solid_add {topoDS_shape} -- the solid to be assembled on solid_base
+        solid_base {topoDS_shape} -- the solid which the solid_add assembled on
 
     Keyword Arguments:
-        tolerance {float} -- content: float    vec = [float(i) for i in ang_list['minPair'][0][1].split(',')]
-                             the angle within this tolerance [deg] will be consider as the same set. (default: {0.5})
+        ang_tol [deg] {float} -- the angle between the normal of planes within ang_tol [deg] will be considered as the same pair. (default: {0.5})
 
     Returns:
-        {dictionary} -- content: {string:TopoDS_Shape}
+        {dictionary} -- 'minVal': {float} [deg] minimal angle between normals of planes
+                        'minDirPair': {tuple of float} direction of normal
     """
     # should consider Z axis first?
-    if normal_base is None:
+    normal_base_withPlanes = group_planes_by_axis(solid1)
+    normal_add_withPlanes = group_planes_by_axis(solid2)
+
+    if normal_base_withPlanes is None:
         plnXY = gp_Pln(gp_Ax3(gp_Pnt(0.0, 0.0, 0.0), gp_Dir(0.0, 0.0, 1.0)))
         plnYZ = gp_Pln(gp_Ax3(gp_Pnt(0.0, 0.0, 0.0), gp_Dir(1.0, 0.0, 0.0)))
         plnZX = gp_Pln(gp_Ax3(gp_Pnt(0.0, 0.0, 0.0), gp_Dir(0.0, 1.0, 0.0)))
-        normal_base = {'0.000,0.000,1.000': [BRepBuilderAPI_MakeFace(plnXY).Face()],
-                       '1.000,0.000,0.000': [BRepBuilderAPI_MakeFace(plnYZ).Face()],
-                       '0.000,1.000,0.000': [BRepBuilderAPI_MakeFace(plnZX).Face()]}
-    keys_base = normal_base.keys()
-    keys_add = normal_add.keys()
-    tol = tolerance  # [deg]
-    minPair = {'minVal': 90.0, 'minPair': []}
+        normal_base_withPlanes = {(0, 0, 1): [BRepBuilderAPI_MakeFace(plnXY).Face()],
+                                  (1, 0, 0): [BRepBuilderAPI_MakeFace(plnYZ).Face()],
+                                  (0, 1, 0): [BRepBuilderAPI_MakeFace(plnZX).Face()]}
+    normals_base = normal_base_withPlanes.keys()
+    normals_add = normal_add_withPlanes.keys()
+    minPair = {'minVal': 90.0}
 
-    for key_base in keys_base:
-        surf_base = BRepAdaptor_Surface(normal_base[key_base][0], True)
-        ax1_normal_base = surf_base.Plane().Axis()
-        for key_add in keys_add:
-            surf_add = BRepAdaptor_Surface(normal_add[key_add][0], True)
-            ax1_normal_add = surf_add.Plane().Axis()
-            ang = degrees(ax1_normal_base.Angle(ax1_normal_add))
+    for normal_base in normals_base:
+        dir_base = gp_Dir(normal_base[0], normal_base[1], normal_base[2])
+        for normal_add in normals_add:
+            dir_add = gp_Dir(normal_add[0], normal_add[1], normal_add[2])
+            ang = degrees(dir_base.Angle(dir_add))
             complementary_angle = 180.0 - ang
             min_ang = min(ang, complementary_angle)
-            if abs(min_ang - minPair['minVal']) <= tol:
-                minPair['minPair'].append([key_base, key_add])
+            if abs(min_ang - minPair['minVal']) <= ang_tol:
+                minPair['minGpDirPair'].append([dir_base, dir_add])
+                minPair['minAxisKeyPair'].append([normal_base, normal_add])
+
             else:
-                if minPair['minVal'] - min_ang >= tol:
-                    minPair['minPair'] = [[key_base, key_add]]
+                if minPair['minVal'] - min_ang >= ang_tol:
+                    minPair['minGpDirPair'] = [[dir_base, dir_add]]
+                    minPair['minAxisKeyPair'] = [[normal_base, normal_add]]
                     minPair['minVal'] = min_ang
     return minPair
+
+
+def align_planes_byNormal(shp_add, normalDir_base, normalDir_add, rotateAng):
+    """[summary]
+
+    Arguments:
+        shp_add {topoDS_Shape} -- [description]
+        normalDir_base {gp_Dir} -- [description]
+        normalDir_add {gp_Dir} -- [description]
+        rotateAng [deg]{float} -- [description]
+    """
+    rotateAxDir = normalDir_base.Crossed(normalDir_add)
+    rotateAx1 = gp_Ax1(pnt2, rotateAxDir)
+    ax3 = gp_Ax3(gp_Pnt(0, 0, 0), gp_Dir(0, 0, 1))
+    ax3 = ax3.Rotated(rotateAx1, radians(rotateAng))
+    shp2Trsf = gp_Trsf()
+    shp2Trsf.SetTransformation(ax3)
+    shp2Toploc = TopLoc_Location(shp2Trsf)
+    shp_add.Move(shp2Toploc)
+
+
+def get_closest_parallel_planePair(solid_base, solid_add, init_min_dist=10.0):
+    min_dist = init_min_dist
+    ang_list = find_closest_normal_pair(solid_add, solid_base)
+    axisGrp1 = group_planes_by_axis(solid1)
+    axisGrp2 = group_planes_by_axis(solid2)
+    axisPair = ang_list['minAxisKeyPair']
+    for axKeyPair in axisPair:
+        for topoPln1 in axisGrp1[axKeyPair[0]]:
+            surf1 = BRepAdaptor_Surface(topoPln1, True)
+            pln1 = surf1.Plane()
+            # [ToDo] A better distance evaluation, how to select a correct point which is inside the wire or centerofmass of plane
+            plnpnt1 = pln1.Location()
+            for topoPln2 in axisGrp2[axKeyPair[1]]:
+                surf2 = BRepAdaptor_Surface(topoPln2, True)
+                pln2 = surf2.Plane()
+                print('Distance:', pln2.Distance(plnpnt1))
+                dist = pln2.Distance(plnpnt1)
+                if dist < min_dist:
+                    min_dist = dist
+                    minDistTopoPair = [topoPln1, topoPln2]
+                    minDistGeomPair = [pln1, pln2]
+    p = gp_Pnt(0, 0, 0)
+    p1 = ais_ProjectPointOnPlane(p, minDistGeomPair[0])
+    p2 = ais_ProjectPointOnPlane(p, minDistGeomPair[1])
+    # in order to remove small digits
+    projPln = minDistGeomPair[0]
+    mvVec = gp_Vec(p2, p1)
+    projPlnNormal = gp_Vec(projPln.Axis().Direction())
+    mag = np.sign(mvVec.Dot(projPlnNormal)) * mvVec.Magnitude()
+    mvVec = projPlnNormal.Normalized().Multiplied(mag)
+    return {'minDist': dist, 'topoPair': minDistTopoPair, 'geomPair': minDistGeomPair, 'mvVec': mvVec}
+
+
+def align_closest_planes(shp, mvVec):
+    shp2Trsf = gp_Trsf()
+    shp2Trsf.SetTranslation(mvVec)
+    shp2Toploc = TopLoc_Location(shp2Trsf)
+    shp.Move(shp2Toploc)
 
 
 if __name__ == '__main__':
@@ -143,92 +201,34 @@ if __name__ == '__main__':
     pnt1 = centerOfMass(solid1)
     pnt2 = centerOfMass(solid2)
 
-    normal_group1 = group_planes_by_axis(solid1)
-    normal_group2 = group_planes_by_axis(solid2)
-    ang_list = find_closest_normal_pair(normal_group2, normal_base=normal_group1)
-
     frame = Display(solid1, run_display=True)
     frame.add_shape(solid2)
     frame.open()
+    ipdb.set_trace(context=10)
+    # find normal of closest plane pairs
+    ang_list = find_closest_normal_pair(solid_add=solid2, solid_base=solid1)
+
     # Get rotation axis
     # [ToDo] the angle value related to rotation direction is not define clearly
-    # [ToDo] adapt a, b with the vector got from ang_list
     # [ToDo] Add condition: if after rotation the angle between two axis is bigger then reverse one axis
     # [ToDo] use While loop to rotate until angle smaller than given angle tolerance
+    # need to weight by the area of surface
 
-    a = list2gpVec([float(i) for i in ang_list['minPair'][0][0].split(',')])
-    b = list2gpVec([float(i) for i in ang_list['minPair'][0][1].split(',')])
-    c = a.Crossed(b)
-    rev_ax = gp_Dir(c)
-    ax1 = gp_Ax1(pnt2, rev_ax)
-    ax3 = gp_Ax3(gp_Pnt(0, 0, 0), gp_Dir(0, 0, 1))
-    ax3 = ax3.Rotated(ax1, radians(float(ang_list['minVal'])))
-    shp2Trsf = gp_Trsf()
-    shp2Trsf.SetTransformation(ax3)
-    shp2Toploc = TopLoc_Location(shp2Trsf)
-    solid2.Move(shp2Toploc)
-
-    ipdb.set_trace(context=10)
+    # here we only align the first pair
+    align_planes_byNormal(solid2, ang_list['minGpDirPair'][0][0], ang_list['minGpDirPair'][0][1], rotateAng=ang_list['minVal'])
 
     # Plane distance detection
     # [ToDo] a better algorithm to choose the correct plane is needed
-    normal_group1 = group_planes_by_axis(solid1)
-    normal_group2 = group_planes_by_axis(solid2)
 
-    min_dist = 10.0
-    ang_list = find_closest_normal_pair(normal_group2, normal_base=normal_group1)
-    # [ToDo] A better distance evaluation, how to select a correct point which is inside the wire
-    for topoPln1 in normal_group1[ang_list['minPair'][0][0]]:
-        surf1 = BRepAdaptor_Surface(topoPln1, True)
-        pln1 = surf1.Plane()
-        plnpnt1 = pln1.Location()
-
-        for topoPln2 in normal_group2[ang_list['minPair'][0][1]]:
-            surf2 = BRepAdaptor_Surface(topoPln2, True)
-            pln2 = surf2.Plane()
-            print('Distance:', pln2.Distance(plnpnt1))
-            min_dist = min(pln2.Distance(plnpnt1), min_dist)
+    minDistPair = get_closest_parallel_planePair(solid2, solid1)
 
     # Move plane to be coincident
     # [ToDo] vector direction need to be determined...
     # [ToDo] while loop to move until distance smaller than given distance tolerance
-    ax3 = gp_Ax3()
-    shp2Trsf = gp_Trsf()
-    dirList = [float(i) for i in ang_list['minPair'][0][0].split(',')]
-    mvVec = gp_Vec(dirList[0], dirList[1], dirList[2]).Normalized().Multiplied(-1.0 * min_dist)
-    shp2Trsf.SetTranslation(mvVec)
-    shp2Toploc = TopLoc_Location(shp2Trsf)
-    solid2.Move(shp2Toploc)
+    align_closest_planes(solid2, minDistPair['mvVec'])
 
-    ipdb.set_trace()
-
-    # Another mether to align the normal of plane: too complicated
-    '''
-    # extract normal vector into a list
-    toVec = [float(i) for i in ang_list['minPair'][0][0].split(',')]
-    xVec = [1.0, 0.0, 0.0]
-    fromVec = [float(i) for i in ang_list['minPair'][0][1].split(',')]
-    fromDir = gp_Dir(fromVec[0], fromVec[1], fromVec[2])
-    toDir = gp_Dir(toVec[0], toVec[1], toVec[2])
-    toDir = gp_Dir(0.0, 1.0, 0.0)
-    # solid 1 turn to Z
-    turn2Z(solid1, toDir)
-    # solid 2 move to center of Mass
-    mv2CMass(solid2, pnt2)
-    # solid2 turn to Z
-    turn2Z(solid2, toDir)
-    ax3 = gp_Ax3(gp_Pnt(0, 0, 0), toDir)
-    shp2Trsf = gp_Trsf()
-    shp2Trsf.SetTransformation(ax3)
-    pnt2 = pnt2.Transformed(shp2Trsf)
-    # align to Z
-    toDir2 = gp_Dir(-0.088, 0.146, 0.985)
-    turn2Z(solid2, toDir2)
-    mv2CMass(solid2, pnt2.Mirrored(gp_Pnt(0,0,0)))
-    '''
-    ipdb.set_trace(context=10)
-    temp = solid_comp([solid1, solid2])
-    write_step_file(solid_comp([solid1, solid2]), 'lf064-0102_1.stp')
+    # temp = solid_comp([solid1, solid2])
+    # write_step_file(solid_comp([solid1, solid2]), 'lf064-0102_1.stp')
     frame.open()
 
     ipdb.set_trace(context=10)
